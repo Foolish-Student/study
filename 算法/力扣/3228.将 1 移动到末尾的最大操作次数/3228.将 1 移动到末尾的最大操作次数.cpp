8bbcf3402147549abbe4dc4c5b43dd02194f2c8b
huiyuanai <2916329883@qq.com>
2025-11-13 22:51:49 +0800

    feat:3228.将 1 移动到末尾的最大操作次数

---
+++ 算法/力扣/3228.将 1 移动到末尾的最大操作次数/3228.将 1 移动到末尾的最大操作次数.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int maxOperations(string s) {
+        int ans = 0, cnt1 = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') {
+                cnt1++;
+            } else if (i > 0 && s[i - 1] == '1') {
+                ans += cnt1;
+            }
+        }
+        return ans;
+    }
+};
