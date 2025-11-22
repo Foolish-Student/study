7196edf76788a267709ab2e35ed640f39480e23c
huiyuanai <2916329883@qq.com>
2025-11-22 22:01:04 +0800

    feat：717.1比特与2比特字符

---
+++ 算法/力扣/717.1比特与2比特字符/717.1比特与2比特字符.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int i = 0;
+        int n = bits.size();
+        while(i < n - 1){
+            if(bits[i] == 0){
+                i++;
+            }else{
+                i += 2;
+            }
+        }
+        return i == n - 1;
+    }
+};
