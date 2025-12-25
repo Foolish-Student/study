40df9239bb94c3d5b0a4fde63f38cdd435453394
huiyuanai <2916329883@qq.com>
2025-12-25 22:45:24 +0800

    feat:744.寻找比目标字母大的最小字母

---
+++ 算法/力扣/744.寻找比目标字母大的最小字母/744.寻找比目标字母大的最小字母.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        if (letters[n - 1] <= target)
+            return letters[0];
+
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return letters[left];
+    }
+};
